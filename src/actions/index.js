0114de8a14f0500451288b8211bde572a4cfa09e
MCola <mcolapietro502@gmail.com>
2020-06-04 10:13:55 -0400

    completed 1st draft of API pulls for parkinformation, campsiteinformation, eventinformation; exported the returns as const variables

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,18 +1,47 @@
 import axios from 'axios';
 
+export const FETCH_PARK = 'FETCH_PARK';
+export const FETCH_CAMPSITE = 'FETCH_CAMPSITE';
+export const FETCH_EVENTS = 'FETCH_EVENTS';
+
 const API_KEY = 'oelQSXnAad2Y6AZ2pYuY7Vxh9r7ji2p8bcy1h8M6';
-const ROOT_URL = `https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=${API_KEY}`
+const ROOT_URL = 'https://developer.nps.gov/api/v1/'
 
-export const FETCH_WEATHER = 'FETCH_WEATHER';
+//API pull for Park information
+export function fetchParkInformation(parkCode) {
+  const url = `${ROOT_URL}/parks?parkCode=${parkCode}&api_key=${API_KEY}`
+  const request = axios.get(url);
 
-export function fetchWeather(city) {
-  const url = `${ROOT_URL}&q=${city},us&units=imperial`;
+  console.log('Request', request);
+  //this is the action; need to call the redux store dispatch
+  return {
+    type: FETCH_PARK,
+    payload: request
+  };
+}
+
+//API pull for campsite information
+export function fetchCampSiteInformation(parkCode) {
+  const url = `${ROOT_URL}/campgrounds?parkCode=${parkCode}&api_key=${API_KEY}`
   const request = axios.get(url);
 
   console.log('Request', request);
 
   return {
-    type: FETCH_WEATHER,
+    type: FETCH_CAMPSITE,
+    payload: request
+  };
+}
+
+//API pull for event information
+export function fetchEVENTInformation(parkCode) {
+  const url = `${ROOT_URL}/events?parkCode=${parkCode}&api_key=${API_KEY}`
+  const request = axios.get(url);
+
+  console.log('Request', request);
+
+  return {
+    type: FETCH_EVENTS,
     payload: request
   };
 }
