81c1f4b50f18350fab1e3bea384fac31a54d31ce
Ann Schnabel <annschnabel@Anns-MacBook-Pro.local>
2020-06-03 21:30:12 -0400

    initial file setup. css added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import promise from "redux-promise";
+
+import "./index.css"
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <h1>Hello World</h1>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
