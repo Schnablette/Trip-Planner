a320038ebaf13eac6c34e91cca745245115505ac
MCola <mcolapietro502@gmail.com>
2020-06-04 20:36:43 -0400

    fixed API pulling of event and campsite data

--- src/reducers/reducer-events.js
+++ src/reducers/reducer-events.js
@@ -3,7 +3,12 @@ import { FETCH_EVENTS } from '../actions/index';
 export default function(state = null, action) {
   switch (action.type) {
     case FETCH_EVENTS:
-      return action.payload.data.data[0];
+      if(!action.payload.data.data[0]) {
+        return action.payload.data
+      } else {
+        return action.payload.data.data[0];
+      }
+      
     default:
       return state;
   }
